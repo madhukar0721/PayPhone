a71d1b01ec222fd40f0e5d4a1fcf806884fece4b
Madhukar <madhukarkumarsocial@gmail.com>
2025-02-15 15:51:50 +0530

    added admin Middleware

--- backend/middleware.js
+++ backend/middleware.js
@@ -31,6 +31,36 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
+const adminMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.header("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(403).json({ error: "Please provide a valid token" });
+    }
+    const userToken = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(userToken, JWT_SECRET);
+      console.log(decoded)
+      if (decoded.userId) {
+        req.userId = decoded.userId;
+        next();
+      }
+    } catch (e) {
+      return res.status(401).json({
+        error: "Token Expired",
+      });
+    }
+  } catch (error) {
+    console.error("Internal Server Error:", error);
+    return res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
+
 module.exports = {
   authMiddleware,
+  adminMiddleware,
 };
