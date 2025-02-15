a5087fbded033b29000dc72e8b9da2447077420e
Madhukar <madhukarkumarsocial@gmail.com>
2025-02-15 15:53:45 +0530

    added accountSchema

--- backend/db.js
+++ backend/db.js
@@ -1,52 +1,75 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGO_URL)
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+try {
+  mongoose.connect(process.env.MONGO_URL);
+} catch (error) {
+  console.error("Internal Server Error:", error);
+}
+
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        minLength: 3,
-        maxLength: 30
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    },
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
-    },
-    token:{
-        type: String,
-        default: "",
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
 
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now
-    }
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  token: {
+    type: String,
+    default: "",
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-// Create a model from the schema
-const User = mongoose.model('User', userSchema);
+// Create a User model from the User schema
+const User = mongoose.model("User", userSchema);
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Account = mongoose.model("Account", accountSchema);
 
 module.exports = {
-	User
+  User,
+  Account,
 };
